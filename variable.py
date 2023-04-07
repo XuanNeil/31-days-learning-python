026804a570b86b2b06ba54610c7704751a4a14ea
Neil <dev.haxuan@gmail.com>
2023-04-07 23:18:05 +0700

    init project

---
+++ variable.py
@@ -0,0 +1,14 @@
+#Python không có lệnh khai báo 1 biến
+#1 biến được tạo ngay khi gán giá trị cho nó lần đầu tiên.
+x = 5
+y = 'No Name'
+print(x)
+print(y)
+
+#Biến không cần phải khai báo với kiểu cụ thể nào, và thậm chí có thể thay đổi loại sau khi đã được gán dữ liệu
+z = 4
+z = 'Neil'
+print(z)
+
+#Nếu bạn chỉ thị 1 kiểu dữ liệu của 1 biến, có thể sử dụng với casting
+d = str(3)# d sẽ là '3'
